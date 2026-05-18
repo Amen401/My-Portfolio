131b704e19c3d1efc7d06686f0a259e0d887999c
Pawlos Gelgelo <pawlosgelgelo@gmail.com>
2026-05-18 12:30:18 +0300

    cors update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -21,11 +21,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:5173",
-      "https://paul-gelgelo-portifolio.vercel.app/",
-    ],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
