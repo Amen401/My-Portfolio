d27a1a08e1fc2649e5a7eaa538944fa2354ddfc6
Pawlos Gelgelo <pawlosgelgelo@gmail.com>
2026-07-27 11:30:42 +0300

    some chnages are updated

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -4,14 +4,12 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       serverSelectionTimeoutMS: 5000,
-      // Force these settings for the Atlas handshake
-      tls: true,
-      tlsAllowInvalidCertificates: true,
     });
-    console.log(`MongoDB Connected`);
+
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error("MongoDB Error:", error);
+    throw error;
   }
 };
 
